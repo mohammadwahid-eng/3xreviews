5925db5e9b226aa68fc9fba9fd348207518d6016
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-01-21 21:49:03 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>3x Reviews</title>
+    <link rel="icon" href="favicon.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;800&display=swap">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" crossorigin>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+	<body>
+    <header>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">3xReviews</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Pricing</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Get Started</a></li>
+            </ul>
+            <a href="#" class="btn btn-primary">LOGIN</a>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <main class="py-5">
+      <section class="bg-texture py-md-5">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h1 class="fw-bolder text-center mb-4 text-uppercase display-4">What we do</h1>
+            </div>
+          </div>
+          <div class="row gap-3 gap-md-0">
+            <div class="col-md-8">
+              <p>Every business wants to get more customers...<br>You have to fill your store, or make the phone ring, or both!<br>But it can be hard to truly stand out from the crowd and build trust quickly, right?<br>That's why 3XReviews offers an Automated Reviews Suite that has already helped generate tens of thousands of new calls and customers for local businesses...</p>
+            </div>
+            <div class="col">
+              <blockquote class="quotebox position-relative lh-1 fw-bolder text-center border border-5 border-black mx-auto my-0">"93% of customers check online reviews before choosing a business"</blockquote>
+            </div>
+          </div>
+          <div class="row gap-3 gap-md-0 mt-4 align-items-center">
+            <div class="col-md-8">
+              <p>and it's <strong>GUARANTEED</strong> to <u>work</u> <u>for</u> <u>you</u> too!<br>Ask your local representative about a Free Trial so you can see for yourself <strong class="text-danger"><em>RISK</em> FREE</strong> today!</p>
+              <p>Call Toll Free 1-833 MORE STARS 1-833-667-3782</p>
+            </div>
+            <div class="col">
+              <img src="img/free_trial.png" alt="Free trial" class="img-fluid d-block mx-auto">
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="container pt-5">
+        <div class="row">
+          <div class="col">
+            <h1 class="fw-bolder text-center mb-4">Showcase Your Happy Customers<small class="d-block fw-normal">(on Social Media & Your Website)</small></h1>
+          </div>
+        </div>
+        <div class="row gap-4 gap-sm-0 align-items-center">
+          <div class="col-12 col-sm-6">
+            <img src="img/happy_customers.png" alt="Happy customers" class="img-fluid d-block mx-auto">
+          </div>
+          <div class="col-12 col-sm-6">
+            <p class="m-0 dropcap">📢 Leverage the power of positive reviews! We help you showcase your happy customers on social media and your website, building trust with potential clients and reinforcing your reputation as a reliable business.</p>
+          </div>
+        </div>
+      </section>
+
+      <section class="container pt-5">
+        <div class="row">
+          <div class="col">
+            <h1 class="fw-bolder text-center mb-4">Automatic Customer Feedback Reminders:</h1>
+          </div>
+        </div>
+        <div class="row flex-sm-row-reverse gap-4 gap-sm-0 align-items-center">
+          <div class="col-12 col-sm-6">
+            <img src="img/customer_feedback.png" alt="Customer feedback" class="img-fluid d-block mx-auto">
+          </div>
+          <div class="col-12 col-sm-6">
+            <p class="m-0 dropcap">👍 Encourage happy customers to share their experiences! Our automated feedback reminders prompt satisfied clients to leave positive reviews, enhancing your online presence and attracting new customers.</p>
+          </div>
+        </div>
+      </section>
+
+      <section class="container pt-5">
+        <div class="row">
+          <div class="col">
+            <h1 class="fw-bolder text-center mb-4">A.I. Prewritten Replies:</h1>
+          </div>
+        </div>
+        <div class="row gap-4 gap-sm-0 align-items-center">
+          <div class="col-12 col-sm-6">
+            <img src="img/ai_replies.png" alt="AI replies" class="img-fluid d-block mx-auto">
+          </div>
+          <div class="col-12 col-sm-6">
+            <p class="m-0 dropcap">✏️ Craft the perfect response every time! We provide pre-written replies you can easily edit that are customized to address various types of reviews, saving you time and ensuring your responses are consistent.</p>
+          </div>
+        </div>
+      </section>
+
+      <section class="container pt-5">
+        <div class="row">
+          <div class="col">
+            <h1 class="fw-bolder text-center mb-4">Daily Reputation Monitoring:</h1>
+          </div>
+        </div>
+        <div class="row flex-sm-row-reverse gap-4 gap-sm-0 align-items-center">
+          <div class="col">
+            <img src="img/daily_monitoring.png" alt="Daily monitoring" class="img-fluid d-block mx-auto">
+          </div>
+          <div class="col">
+            <p class="m-0 dropcap">👁️‍🗨️ Stay on top of your online reputation effortlessly! We monitor review platforms daily to ensure you never miss a customer's feedback, allowing you to respond promptly and professionally.</p>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <footer class="bg-black text-white">
+      <section class="container">
+        <div class="row">
+          <div class="col">
+            <h2 class="fw-bolder text-uppercase text-center mb-4 display-5">Let's get you started!</h2>
+          </div>
+        </div>
+        <div class="row gap-3 align-items-center">
+          <div class="col">
+            <p>Ask your local representative about a Free Trial so you can see for yourself <strong class="text-danger"><em>RISK</em> FREE</strong> today!</p>
+            <p>Call Toll Free 1-833 MORE STARS 1-833-667-3782</p>
+          </div>
+          <div class="col-12 col-sm-auto">
+            <img src="img/free_trial.png" alt="Free trial" class="img-fluid mx-auto d-block">
+          </div>
+          <div class="col-12">
+            <p class="my-4 text-center">&copy; Copyright Vloopiter, LLC 2024 | <a href="#" class="text-reset text-decoration-none">Privacy Policy</a> | <a href="#" class="text-reset text-decoration-none">Terms</a></p>
+          </div>
+        </div>
+      </section>
+    </footer>
+
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" crossorigin></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
