f87afc1c125f47573f3b619a8c5ffb09e88809b9
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-01-24 01:03:11 +0600

    service section added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ new Typed('#typed', {
     loop: true,
     cursorChar: '✏️',
 });
+
+document.addEventListener('mousemove', function(e) {
+    this.querySelectorAll('[data-toggle="parallax"]').forEach(function(elem) {
+        const speed = elem.getAttribute('data-speed') || 1;
+        const x = (window.innerWidth - e.pageX * speed) / 100;
+        const y = (window.innerHeight - e.pageY * speed) / 100;
+        console.log(x, y);
+        elem.style.transform = `translate(${x}px, ${y}px)`;
+    });
+});
